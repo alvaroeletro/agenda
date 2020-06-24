bf2b4b5d87cadf43951aa3b6b5304419256b2e55
alvaroeletro <alvaro_eletro@hotmail.com>
2020-06-24 18:51:46 -0300

    url/Agenda traz listagem dos eventos criados em /Admin

--- core/views.py
+++ core/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, redirect
+from core.models import Evento
+
+
+
 
 # Create your views here.
+def index(request):
+    return redirect('/agenda')
+
+def lista_eventos (request):
+    usuario = request.user
+    evento = Evento.objects.all()
+    #mostra somente os eventos do usuário
+    data = {'event': evento}
+    return render(request, 'agenda.html', data)
+
+
+
+
+
+
+
