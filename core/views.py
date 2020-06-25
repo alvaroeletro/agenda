412ade069742e9f96ded4e098fb0740efedaa05a
alvaroeletro <alvaro_eletro@hotmail.com>
2020-06-25 19:14:32 -0300

    Login e logout funcionando. Listagem para usuário

--- core/views.py
+++ core/views.py
@@ -1,19 +1,38 @@
 from django.shortcuts import render, redirect
 from core.models import Evento
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+
+def login_user (request):
+    return render(request, 'login.html')
+
+def submit_login(request):
+    if request.POST:
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        usuario = authenticate(username=username, password=password)
+        if usuario   is not None :
+            login (request, usuario)
+            return redirect('/')
+        else:
+            messages.error(request, "Usuário ou senha inválido")
+    return redirect('/')
+
+def logout_user (request):
+    logout(request)
+    return redirect("/")
 
 
-
-
-# Create your views here.
+@login_required(login_url='/login/')
+def lista_eventos (request):
+       user = request.user
+       evento = Evento.objects.filter(usuario=user)
+       #mostra somente os eventos do usuário
+       data = {'event': evento}
+       return render(request, 'agenda.html', data)
 def index(request):
     return redirect('/agenda')
-
-def lista_eventos (request):
-    usuario = request.user
-    evento = Evento.objects.all()
-    #mostra somente os eventos do usuário
-    data = {'event': evento}
-    return render(request, 'agenda.html', data)
 
 
 
